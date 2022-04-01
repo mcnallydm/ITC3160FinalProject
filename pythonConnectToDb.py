2e878e27763d32920434fd2c52eccdedfa56798d
Dara McNally (267982) <s-dm267982@acgadmin.edu>
2022-04-01 12:15:53 +0300

    fixed printquery function

--- pythonConnectToDb.py
+++ pythonConnectToDb.py
@@ -4,6 +4,7 @@ database = "@itc3260.acgadmin.edu:1521/orcl"
 username = input("Username: ")
 password = input("Password: ")
 connection_string = username + "/" + password + database
+connected = 1
 try:
     cx_Oracle.init_oracle_client(lib_dir=r"C:\Programs\oracleClient")
     #connection = cx_Oracle.connect('hr/oracle@itc3260.acgadmin.edu:1521/orcl')
@@ -11,25 +12,29 @@ try:
     #print("Login to ", loginInfo, " was successful")
 except:
     print("Login failed.")
+    connected = 0
 
-def print_query(statement):
+def print_query(query_output):
     output = ""
-    for row in results:
+    for row in (query_output):
         for idx in range(0, len(row)):
             if idx==0:
                 output += str(row[idx])
             else:
                 output += " " + str(row[idx])
-        print(output)
-        output = ""
+        output += "\n"
+    output = output[0:len(output)-1]
+    print(output)
 
-# Always define your connection cursor to be able to fetch data from the db
-cursor = connection.cursor()
+if connected:
+    # Always define your connection cursor to be able to fetch data from the db
+    cursor = connection.cursor()
 
-# How to select data from the database
-# Define and execute your select statement
-results = cursor.execute("SELECT employee_id, last_name, first_name, salary FROM dara_emp")
+    # How to select data from the database
+    # Define and execute your select statement
+    select_statement = "SELECT employee_id, last_name, first_name, salary FROM dara_emp"
+    results = cursor.execute(select_statement)
 
-print_query(results)
+    print_query(results)
 
 #TODO: Run "pip install cx_Oracle"
